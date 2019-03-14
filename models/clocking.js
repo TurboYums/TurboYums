f36c9977edc02717d53f505811e6e153976742d8
Anthony <Anthony.Merheb@rutgers.edu>
2019-03-14 16:13:19 -0400

    Imagine actually making progress

--- models/clocking.js
+++ models/clocking.js
@@ -5,13 +5,10 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define('clocking', {
 
     username: Sequelize.STRING,
-    firstname: Sequelize.STRING,
-    lastname: Sequelize.STRING,
-    password: Sequelize.STRING,
-    rewardpoints: Sequelize.INTEGER,
-    accountType: Sequelize.INTEGER,
-    addressId: Sequelize.INTEGER
-  
+    timeIn: Sequelize.STRING,
+    timeOut: Sequelize.STRING,
+    location: Sequelize.String
+
 	/*"Clock Table"
 	Foreign Key to User
 	Time stamp in
