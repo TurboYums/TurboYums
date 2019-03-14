69253a7a697f0ef10d46901598313cd73b3b990b
hes52 <43147748+hes52@users.noreply.github.com>
2019-03-14 14:45:30 -0400

    Create item.js
    Here is the item class for menu

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const sequelize = require('sequelize');
+const sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes)=>
+{
+    return sequelize.define('item',
+    {
+        itemName: Sequelize.STRING,
+        itemPrice: Sequalize.DOUBLE,
+        ingredient[]: Sequalize.STRING,
+        description: Sequalize.STRING,
+        rating: Sequalize.INTEGER,
+        foodId: Sequalize.DOUBLE
+    })
+    
+    
+}
