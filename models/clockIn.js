dd3088670a4537c195c4605e09bd1b7f574ca765
Anthony <Anthony.Merheb@rutgers.edu>
2019-03-14 15:50:43 -0400

    I have 0 clue what I'm doing btw

---
+++ models/clockIn.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./sequelizeConf.js');
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('user', {
+
+    username: Sequelize.STRING,
+    firstname: Sequelize.STRING,
+    lastname: Sequelize.STRING,
+    password: Sequelize.STRING,
+    rewardpoints: Sequelize.INTEGER,
+    accountType: Sequelize.INTEGER,
+    addressId: Sequelize.INTEGER
+  
+	/*"Clock Table"
+	Foreign Key to User
+	Time stamp in
+	Time stamp out
+	*/
+  })
+}
