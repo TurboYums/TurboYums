701c234413df8bbef8ffa97565b7142db50a18a1
hes52 <43147748+hes52@users.noreply.github.com>
2019-03-19 17:29:12 -0400

    Edits
    Edited order and create menu class, no methods are in it yet.

--- models/order.js
+++ models/order.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./sequelizeConf.js');
 
-module.exports = (sequelize, DataTypes)=>
-{
-    return sequelize.define('order',
+Order = sequelize.define('order',
     {
         totalPrice: Sequelize.DOUBLE,
         orderedItems: Sequelize.STRING,
         specialReuest: Sequelize.STRING
     })
+
+module.exports = (sequelize, DataTypes) => {
+    return Order;
 }
 item.belongsToMany(orderedItems);
 orderedItems.belongsToMany(item);
