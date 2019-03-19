37113aa5fb08e2ebf6eb4b3d6b353bcc5ac0e487
DanteT1114 <dst1114@gmail.com>
2019-03-18 20:24:05 -0400

    Major refactor, began to lay out payment

--- server.js
+++ server.js
@@ -1,53 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
+const api = require('./api/api.js');
 const sequelize = require('./models/sequelizeConf.js');
 const User = sequelize.import('./models/user.js');
+const users = require('./api/users.js');
+const port = process.env.port || 5000;
+
 sequelize.sync();
 
-app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-
-app.post('/api/world', (req, res) => {
-  res.send({ text: `I received your POST request. This is what you sent me: ${req.body.text}` });
-});
-
-app.post('/users/create', (req, res) => {
-  newUser = User.create({
-    username: req.body.username,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    password: req.body.password,
-    accountType: req.body.accountType,
-    rewardpoints: 0
-  })
-  res.send({ text: `Created User: ` + newUser.username });
-})
-
-app.post('/users/addpoints', (req, res) => {
-  User.findOne({ where: { username: req.body.username } }).then(user => {
-    console.log("adding points to " + user);
-    user.increment('rewardpoints', {by: req.body.rewardpoints});
-    
-    user.reload().then(() => {
-      res.send({ text: `Gave user: ${req.body.username} , ${user.rewardpoints}` });
-    })
-    
-  })
-
- 
-})
-
-app.post('/paymentMethod/create', (req, res) => {
+api.post('/paymentMethod/create', (req, res) => {
   newPaymentMethod = paymentMethod.create({
     type: req.body.type,
     firstname: req.body.firstname,
@@ -61,4 +20,4 @@ app.post('/paymentMethod/create', (req, res) => {
 })
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+api.listen(port, () => console.log(`Listening on port ${port}`));
