984d2897da50aa7a5eb0ef161cd7353286326c0c
Brandon Tong <bmt76@scarletmail.rutgers.edu>
2019-03-19 00:09:11 -0400

    update1
    added item to server.js and edited item.js

--- models/item.js
+++ models/item.js
@@ -1,18 +1,19 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./sequelizeConf.js');
 
-module.exports = (sequelize, DataTypes)=>
-{
-    return sequelize.define('item',
-    {
+ Item = sequelize.define('item', {
+    
         itemName: Sequelize.STRING,
-        itemPrice: Sequalize.DOUBLE,
-        ingredient: Sequalize.STRING,
-        description: Sequalize.STRING,
-        rating: Sequalize.INTEGER,
-        foodId: Sequalize.DOUBLE,
+        itemPrice: Sequelize.DOUBLE,
+        ingredient: Sequelize.STRING,
+        description: Sequelize.STRING,
+        rating: Sequelize.INTEGER,
+        foodID: Sequelize.DOUBLE,
         itemQuantity: Sequelize.INTEGER
     })
+	
+module.exports = (sequelize, DataTypes) => {
+  return Item;
 }
-item.belongsToMany(ingredient);
-ingredient.belongsToMany(item);
+//item.belongsToMany(ingredient);
+//ingredient.belongsToMany(item);
--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const port = process.env.PORT || 5000;
 
 const sequelize = require('./models/sequelizeConf.js');
 const User = sequelize.import('./models/user.js');
+const Item = sequelize.import('./models/item.js');
 sequelize.sync();
 
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/api/hello', (req, res) => {
@@ -27,9 +28,21 @@ app.post('/users/create', (req, res) => {
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     password: req.body.password,
-    accountType: req.body.accountType,
+    accountType: req.body.accountType
   })
   res.send({ text: `Created User: ${req.body.text}` });
+})
+app.post('/items/create', (req, res) => {
+  newItem = Item.create({
+    itemName: req.body.itemName,
+    itemPrice: req.body.itemPrice,
+    ingredient: req.body.ingredient,
+    description: req.body.description,
+    rating: req.body.rating,
+	foodID: req.body.foodId,
+    itemQuantity: req.body.itemQuantity
+  })
+  res.send({ text: `Created Item: ${req.body.itemName}` });
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
