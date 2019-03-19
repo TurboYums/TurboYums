a7bc518e11352bf2dfc269bed5ade1b7137ffdd1
hes52 <43147748+hes52@users.noreply.github.com>
2019-03-19 19:04:55 -0400

    Update order.js

--- api/order.js
+++ api/order.js
@@ -1,10 +1,10 @@
 const api = require('./api.js');
 const sequelize = require('../models/sequelizeConf.js');
-const Item = sequelize.import('../models/order.js');
+const Order = sequelize.import('../models/order.js');
 sequelize.sync();
 
 api.post('/api/order/create', (req, res) => {
-  newOrder = Item.create({
+  newOrder = Order.create({
         totalPrice: Sequelize.DOUBLE,
         orderedItems: Sequelize.Item,
         specialReuest: Sequelize.STRING
