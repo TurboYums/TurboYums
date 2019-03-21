1cdc74d28e6dbcb33f1e06660a0843e17e2214a9
Ziad Mallah <ziad.mallah@iristt.com>
2019-03-21 16:37:40 -0400

    created login functionality and hashing passwords

--- api/users.js
+++ api/users.js
@@ -3,24 +3,43 @@ const sequelize = require('../models/sequelize.js');
 const User = sequelize.import('../models/user.js');
 const config = require('../config.json');
 const stripe = require('stripe')(config.stripe.STRIPE_SECRET_KEY);
+const bcrypt = require('bcrypt');
 
 api.post('/api/users/create', (req, res) => {
-  stripe.customers.create({
-    email: req.body.email,
-  }).then(customer => {
-    User.create({
-      username: req.body.username,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      password: req.body.password,
-      accountType: req.body.accountType,
-      rewardpoints: 0,
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    stripe.customers.create({
       email: req.body.email,
-      stripe_id: `${customer.id}`
-    }).then(newUser => {
-      res.send({ text: `Created User:  ${newUser.username}` });
+    }).then(customer => {
+      User.create({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: hash,
+        accountType: req.body.accountType,
+        rewardpoints: 0,
+        email: req.body.email,
+        stripe_id: `${customer.id}`
+      }).then(newUser => {
+        res.send({ text: `Created User:  ${newUser.username}` });
+      })
     })
-    
+  })
+})
+
+api.post('/api/users/login', (req, res) => {
+  User.findOne({ where: { username: req.body.username } }).then(user => {
+    bcrypt.compare(req.body.password, user.password, function (err, success) {
+      if (success) {
+        res.send({
+          user: user,
+          loginvalid: true
+        })
+      } else {
+        res.send({
+          loginValid: false
+        })
+      }
+    });
   })
 })
 
