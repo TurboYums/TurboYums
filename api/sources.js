bc86763b3d315392ad9c8e9046240e88838de58f
DanteT1114 <dst1114@gmail.com>
2019-03-23 16:33:35 -0400

    added an ugly payment interface, fixed source

--- api/sources.js
+++ api/sources.js
@@ -8,31 +8,33 @@ const stripe = require('stripe')(config.stripe.STRIPE_SECRET_KEY);
 const Source = sequelize.import('../models/source.js');
 
 api.post('/api/sources/create', (req, res) => {
-
+  console.log('here 11')
   stripe.tokens.create({
     card: {
       number: req.body.number || '4242424242424242',
-      exp_month: req.body.exp_month | 12,
-      exp_year: req.body.exp_year | 2020,
+      exp_month: req.body.exp_month || 12,
+      exp_year: req.body.exp_year || 2020,
       cvc: req.body.cvc || '123'
     }
   }, function (err, token) {
+    console.log(req.body.user)
     stripe.customers.createSource(
-      req.body.user_stripe_id,
+      req.body.user.stripe_id,
       { source: token.id },
       function (err, card) {
+        console.log('here 24')
         Source.create({
           stripe_id: card.id,
-          user_stripe_id: req.body.user_stripe_id,
+          user_stripe_id: req.body.user.stripe_id,
 
           type: req.body.type,
-          firstname: req.body.firstname,
-          lastname: req.body.lastname,
+          firstname: req.body.user.firstname,
+          lastname: req.body.user.lastname,
           billingAddress: req.body.billingAddress,
-          cvv: req.body.cvv,
+          cvv: req.body.cvc,
           expDate: req.body.expDate,
         }).then(newSource => {
-          res.send({ text: `Payment method created, payment method: ` + newSource.stripe_id });
+          res.send({source: newSource});
         })
       }
     )
--- client/App.js
+++ client/App.js
@@ -4,8 +4,9 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'; // 
 import { Alert, AppRegistry, StyleSheet, TouchableNativeFeedback, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
 
 
-const API_URL = 'http://192.168.1.23:5000/';
+const API_URL = 'http://192.168.1.246:5000/';
 let currentUser = ' ';
+let token = '';
 
 class HomeScreen extends React.Component {
   state = {
@@ -44,10 +45,10 @@ class HomeScreen extends React.Component {
 
             <TouchableOpacity
               style={styles.submitButton}
-              onPress={() =>{
-                if(!this.state.username || !this.state.password){
+              onPress={() => {
+                if (!this.state.username || !this.state.password) {
                   Alert.alert('Please enter a username and password.');
-                }else{
+                } else {
                   fetch(API_URL + 'api/users/login', {
                     method: 'POST',
                     headers: {
@@ -59,8 +60,9 @@ class HomeScreen extends React.Component {
                       password: this.state.password,
                     }),
                   }).then((res) => res.json()).then(resJson => {
-                    if(resJson.loginValid){
-                      switch(resJson.user.accountType){
+                    if (resJson.loginValid) {
+                      currentUser = resJson.user;
+                      switch (resJson.user.accountType) {
                         //employee is 0 
                         case 0:
                           currentUser = this.state.username;
@@ -68,15 +70,15 @@ class HomeScreen extends React.Component {
                           break;
                         //customer is 1
                         case 1:
-                          currentUser = this.state.username;
-                          Alert.alert('We have not yet implemented the customer interface!');
+                          this.props.navigation.navigate('Payment');
+                          break;
                       }
-                    }else{
+                    } else {
                       Alert.alert('Incorrect Username or Password.');
                     }
                   });
                 }
-              } }>
+              }}>
               <Text style={styles.submitButtonText}> SUBMIT </Text>
             </TouchableOpacity>
           </ImageBackground>
@@ -94,31 +96,8 @@ class ClockInOutScreen extends React.Component {
           <ImageBackground source={{ uri: 'https://cdn.shopify.com/s/files/1/2398/3757/products/Pizza-Cloth-Front_1024x1024.jpg?v=1512582067' }} style={{ width: '100%', height: '100%' }}>
             <TouchableOpacity
               style={styles.button}
-              onPress={() =>{
-                  fetch(API_URL + 'api/users/clockIn', {
-                    method: 'POST',
-                    headers: {
-                      Accept: 'application/json',
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                      username: currentUser,
-                    }),
-                  }).then((res) => res.json()).then(resJson => {
-                    if(resJson.clockInSuccess){
-                      Alert.alert("Successfully Clocked In: " + currentUser);
-                    } else{
-                      Alert.alert(currentUser + " is already Clocked In!");
-                    }
-                  });
-                }
-              } >
-              <Text style={styles.buttonText}> Clock In </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() =>{
-                fetch(API_URL + 'api/users/clockOut', {
+              onPress={() => {
+                fetch(API_URL + 'api/users/clockIn', {
                   method: 'POST',
                   headers: {
                     Accept: 'application/json',
@@ -128,14 +107,37 @@ class ClockInOutScreen extends React.Component {
                     username: currentUser,
                   }),
                 }).then((res) => res.json()).then(resJson => {
-                  if(resJson.clockOutSuccess){
-                    Alert.alert("Successfully Clocked Out: " + currentUser);
-                  } else{
-                    Alert.alert(currentUser + " is already Clocked Out!");
+                  if (resJson.clockInSuccess) {
+                    Alert.alert("Successfully Clocked In: " + currentUser);
+                  } else {
+                    Alert.alert(currentUser + " is already Clocked In!");
                   }
                 });
               }
-            } >
+              } >
+              <Text style={styles.buttonText}> Clock In </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                fetch(API_URL + 'api/users/clockOut', {
+                  method: 'POST',
+                  headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    username: currentUser.username,
+                  }),
+                }).then((res) => res.json()).then(resJson => {
+                  if (resJson.clockOutSuccess) {
+                    Alert.alert("Successfully Clocked Out: " + currentUser.username);
+                  } else {
+                    Alert.alert(currentUser.username + " is already Clocked Out!");
+                  }
+                });
+              }
+              } >
               <Text style={styles.buttonText}> Clock Out </Text>
             </TouchableOpacity>
           </ImageBackground>
@@ -145,11 +147,190 @@ class ClockInOutScreen extends React.Component {
   }
 }
 
+class PaymentScreen extends React.Component {
+  state = {
+    cardNumber: '',
+    exp_month: '',
+    exp_year: '',
+    cvc: '',
+    postalCode: '',
+  }
+  handlecardNumber = (text) => {
+    this.setState({ cardNumber: text })
+    if (this.state.cardNumber.length < 16) {
+    }
+  }
+  handleexp_month = (text) => {
+    this.setState({ exp_month: text })
+  }
+  handleexp_year = (text) => {
+    this.setState({ exp_year: text })
+  }
+  handlecvc = (text) => {
+    this.setState({ cvc: text })
+  }
+  handlepostalCode = (text) => {
+    this.setState({ postalCode: text })
+  }
+  login = (username, word) => {
+    alert('username: ' + username + ' password: ' + password)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.text}>
+            Enter required fields:
+          </Text>
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   Card Number"
+            autoCapitalize="none"
+            onChangeText={this.handlecardNumber} />
 
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   expiration month"
+            autoCapitalize="none"
+            onChangeText={this.handleexp_month} />
+
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   expiration year ="
+            autoCapitalize="none"
+            onChangeText={this.handleexp_year} />
+
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   cvc"
+            autoCapitalize="none"
+            onChangeText={this.handlecvc} />
+
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   postal code"
+            autoCapitalize="none"
+            onChangeText={this.handlepostalCode} />
+
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={() => {
+              if (!this.state.cardNumber || !this.state.exp_month || !this.state.exp_year || !this.state.cvc || !this.state.postalCode) {
+                Alert.alert('Please enter information for all of the fields please.');
+              } else {
+                fetch(API_URL + 'api/sources/create', {
+                  method: 'POST',
+                  headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    number: this.state.cardNumber,
+                    exp_month: this.state.exp_month,
+                    exp_year: this.state.exp_year,
+                    cvc: this.state.cvc,
+                    user: currentUser
+                  }),
+                }).then((res) => res.json()).then(resJson => {
+                  if (resJson.source) {
+                    fetch(API_URL + "api/charges/create", {
+                      method: 'POST',
+                      headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({
+                        amount: '5544',
+                        currency: 'usd',
+                        source: resJson.source,
+                        description: 'The charge for this one',
+                        customer: currentUser.stripe_id
+                      }),
+                    }).then((res) => res.json()).then(resJson => {
+                      this.props.navigation.navigate('Receipt');
+                    })
+                  }
+                })
+              }
+            }}>
+            <Text style={styles.submitButtonText}> SUBMIT </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+class ReceiptScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.text}>
+            Receipt:
+          </Text>
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   Username"
+            autoCapitalize="none"
+            onChangeText={this.handleusername} />
+
+          <TextInput style={styles.input}
+            underlineColorAndroid="transparent"
+            secureTextEntry={true}
+            placeholder="   Password"
+            autoCapitalize="none"
+            onChangeText={this.handlePassword} />
+
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={() => {
+              if (!this.state.username || !this.state.password) {
+                Alert.alert('Please enter a username and password.');
+              } else {
+                fetch(API_URL + 'api/users/login', {
+                  method: 'POST',
+                  headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    username: this.state.username,
+                    password: this.state.password,
+                  }),
+                }).then((res) => res.json()).then(resJson => {
+                  if (resJson.loginValid) {
+                    currentUser = resJson.user;
+                    switch (resJson.user.accountType) {
+                      //employee is 0 
+                      case 0:
+                        currentUser = this.state.username;
+                        this.props.navigation.navigate('ClockInOut');
+                        break;
+                      //customer is 1
+                      case 1:
+                        this.props.navigation.navigate('Payment');
+                        break;
+                    }
+                  } else {
+                    Alert.alert('Incorrect Username or Password.');
+                  }
+                });
+              }
+            }}>
+            <Text style={styles.submitButtonText}> SUBMIT </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     ClockInOut: ClockInOutScreen,
+    Payment: PaymentScreen,
+    Receipt: ReceiptScreen
   },
   {
     initialRouteName: 'Home',
@@ -163,7 +344,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginTop: 100,
-    color: 'red',
+    color: '#f7df1e',
     margin: 10,
     fontSize: 20,
   },
@@ -171,11 +352,11 @@ const styles = StyleSheet.create({
     margin: 15,
     backgroundColor: 'white',
     height: 40,
-    borderColor: 'red',
+    borderColor: '#f7df1e',
     borderWidth: 1
   },
   submitButton: {
-    backgroundColor: 'red',
+    backgroundColor: '#f7df1e',
     padding: 10,
     margin: 15,
     height: 40,
@@ -189,7 +370,7 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     width: 260,
     alignItems: 'center',
-    backgroundColor: 'yellow'
+    backgroundColor: '#f7df1e'
   },
   buttonText: {
     padding: 20,
