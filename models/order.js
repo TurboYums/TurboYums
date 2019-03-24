4210ad5360ff158ebb6cb45584e929e357f95374
Roshni <roshnishah318@gmail.com>
2019-03-23 23:33:42 -0400

    fixed another merge conflict

--- models/order.js
+++ models/order.js
@@ -1,28 +1,15 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./sequelize.js');
-<<<<<<< HEAD
-const Item = sequelize.import('../models/item.js');
-
-=======
 
 const Item = sequelize.import('../models/item.js');
 const OrderItem = sequelize.define('order_item'); 
 
->>>>>>> c70180b3139ae14e347389fee7a009f8c6a99a85
 Order = sequelize.define('order', {
   totalPrice: Sequelize.DOUBLE,
   specialReuest: Sequelize.STRING
 })
-<<<<<<< HEAD
-
-Order.belongsToMany(Item, { through: 'OrderItem' });
-module.exports = (sequelize, DataTypes) => {
-  return Order;
-}
-=======
 Order.belongsTo(User);
 Order.belongsToMany(Item, { through: OrderItem });
 module.exports = (sequelize, DataTypes) => {
   return Order;
 }
->>>>>>> c70180b3139ae14e347389fee7a009f8c6a99a85
