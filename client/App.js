e8f4a091b10d3ef0077830f7c7b0f865648b7633
Suvranil Ghosh <Suvranil@nbp-139-98.nbp.ruw.rutgers.edu>
2019-04-19 11:24:30 -0400

    lolok

--- client/App.js
+++ client/App.js
@@ -6,14 +6,69 @@ import { Header } from 'react-native-elements';
 import MenuItem from './components/MenuItem';
 import { Ionicons } from '@expo/vector-icons';
 import { unregisterTaskAsync } from 'expo-background-fetch';
+import BottomNavigation, { FullTab } from 'react-native-material-bottom-navigation';
 
-const API_URL = 'http://172.31.236.119:5000/';
+const API_URL = 'http://172.31.139.98:5000/';
 let currentUser = '';
 let order = '';
 let token = '';
 let items = '';
 let employees = '';
 let currentItem = '';
+
+export default class App extends React.Component {
+  tabs = [
+    {
+      key: 'games',
+      icon: 'gamepad-variant',
+      label: 'Games',
+      barColor: '#388E3C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'movies-tv',
+      icon: 'movie',
+      label: 'Movies & TV',
+      barColor: '#B71C1C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'music',
+      icon: 'music-note',
+      label: 'Music',
+      barColor: '#E64A19',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    }
+  ]
+ 
+  renderIcon = icon => ({ isActive }) => (
+    <Icon size={24} color="white" name={icon} />
+  )
+ 
+  renderTab = ({ tab, isActive }) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={this.renderIcon(tab.icon)}
+    />
+  )
+ 
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          {/* Your screen contents depending on current tab. */}
+        </View>
+        <BottomNavigation
+          onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+          renderTab={this.renderTab}
+          tabs={this.tabs}
+        />
+      </View>
+    )
+  }
+}
 
 class LogoTitle extends React.Component {
   render() {
