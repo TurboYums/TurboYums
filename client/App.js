7e14ed6c6b30cf7e3a4e4b1d3f0c3c61743525dd
sg1239 <sg1239@scarletmail.rutgers.edu>
2019-04-19 19:53:46 -0400

    eeeeee

--- client/App.js
+++ client/App.js
@@ -7,7 +7,7 @@ import MenuItem from './components/MenuItem';
 import { Ionicons } from '@expo/vector-icons';
 import { unregisterTaskAsync } from 'expo-background-fetch';
 
-const API_URL = 'http://172.31.149.49:5000/';
+const API_URL = 'http://192.168.1.193:5000/';
 let currentUser = '';
 let order = '';
 let token = '';
@@ -67,7 +67,7 @@ class WelcomeScreen extends React.Component {
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                  style={styles.guestbutton}
+                  style={styles.signUpButton}
                   onPress={() => {
                     this.props.navigation.navigate('Menu');
                   }
